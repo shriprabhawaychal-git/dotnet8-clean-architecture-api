ae3c54a74fba80443caaea5f7d0ec4dfd1fea6cc
shriprabhawaychal-git <shriprabha.waychal@gmail.com>
2026-03-10 14:08:33 -0400

    Add EF Core DbContext and initial database migration

--- CleanArchitecture.Api/Program.cs
+++ CleanArchitecture.Api/Program.cs
@@ -1,3 +1,5 @@
+using CleanArchitecture.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.Api
 {
@@ -13,6 +15,8 @@ namespace CleanArchitecture.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
---
+++ CleanArchitecture.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
